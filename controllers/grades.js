58c22cce28b65e239b926600d0b17fa8e7fd0709
Zyad Abdul-Nasser <zizo_007@live.com>
2024-01-09 02:04:51 +0200

    z

--- controllers/grades.js
+++ controllers/grades.js
@@ -1,10 +1,39 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteGrade = exports.getGrades = exports.addGrade = void 0;
-const NotFoundError_1 = __importDefault(require("../errors/NotFoundError"));
+const db = __importStar(require("../db/index"));
 const addGrade = (req, res) => {
     const name = req.body.name;
     console.log(req.body);
@@ -12,15 +41,11 @@ const addGrade = (req, res) => {
     res.status(201).send({ message: 'added grade successfully' });
 };
 exports.addGrade = addGrade;
-const getGrades = (req, res) => {
-    if (1 === 1) {
-        throw new NotFoundError_1.default('Error GET grades', 1);
-    }
-    res.status(200).send([
-        { name: "Zyad" },
-        { name: "Anazz" }
-    ]);
-};
+const getGrades = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { rows } = yield db.query('SELECT * FROM grade', []);
+    console.log(rows);
+    res.send();
+});
 exports.getGrades = getGrades;
 const deleteGrade = (req, res) => {
     console.log(req.params.id);
